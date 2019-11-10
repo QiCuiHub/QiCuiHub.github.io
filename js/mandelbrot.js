1982146eb5a5f84aa0591868ad121b583ec48e6e
QiCuiHub <qi.cui@mail.utoronto.ca>
2019-11-10 14:37:06 -0500

    update personal website

---
+++ js/mandelbrot.js
@@ -0,0 +1,227 @@
+// Split double into two singles
+let split = (a) => {
+    let hi = Math.fround(a);
+    let lo = a - hi;
+    
+    return [hi, lo];
+}
+
+// State
+const resolution = {
+    width: 600,
+    height: 400
+}
+
+const state = {
+    center : [-(resolution.height / resolution.width), 0.0],
+    scale  : 0.005
+}
+    
+const uniforms = {
+    center     : [...split(state.center[0]), ...split(state.center[1])],
+    offset     : [resolution.width / 2.0, resolution.height / 2.0],
+    scale      : split(state.scale),
+    zero       : [0.00000000000001, 0.00000000000001],
+    iterations : 768.0,
+};
+
+// App
+const app = new PIXI.Application(resolution);
+const hammer = new Hammer(app.view);
+hammer.add( new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 }) );
+hammer.add( new Hammer.Pinch({ threshold: 0 }) );
+
+document.getElementById("app").appendChild(app.view);
+
+// Check Available Presicion and Renderer
+const gl = app.renderer.context.gl;
+const precision = gl.getShaderPrecisionFormat(gl.FRAGMENT_SHADER, gl.HIGH_FLOAT).precision;
+const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+const renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+
+// Canvas sized Quad
+const geometry = new PIXI.Geometry()
+    .addAttribute('aVertexPosition',
+        [-resolution.width, -resolution.height, 
+          resolution.width, -resolution.height,
+          resolution.width,  resolution.height,
+         -resolution.width,  resolution.height], 
+        2) 
+    .addIndex([0, 1, 2, 0, 2, 3]);
+
+// Add to canvas
+const shader = PIXI.Shader.from(vertex, fragment, uniforms);
+const quad = new PIXI.Mesh(geometry, shader, uniforms);
+const coords = new PIXI.Text('', {fontFamily : 'Arial', fontSize: 12, fill : 0x00ff00, align : 'left'});
+quad.interactive = true;
+
+app.stage.addChild(quad);
+app.stage.addChild(coords);
+
+// Movement
+let keyState = {};
+let mousedown = false;
+let mouseover = false;
+
+let zoom = 0.99;
+let scrollZoom = 0.84;
+let movementSpeed = 4 * state.scale;
+let panSens = 1 - (resolution.height / resolution.width);
+let showDebug = false;
+
+let prevCoord = null;
+let baseScale = null;
+let baseMs = null
+
+window.addEventListener('keydown', (e) => {
+    keyState[e.keyCode || e.which] = true;
+    if (e.keyCode == 68 || e.which == 68) showDebug = !showDebug;
+    app.ticker.start();
+}, true);
+
+window.addEventListener('keyup', (e) => {
+    delete keyState[e.keyCode || e.which];
+    
+    // Stop rendering if no keys pressed and mouse not over canvas
+    if (Object.keys(keyState).length == 0 && !mouseover) app.ticker.stop();
+},true);
+
+window.addEventListener('wheel', (e) => {
+    // only scroll when mouse over canvas
+    if (app.renderer.plugins.interaction.mouseOverRenderer){
+        app.ticker.start();
+ 
+        // zoom in
+        if (Math.sign(e.deltaY) > 0){
+            state.scale /= scrollZoom;
+            movementSpeed /= scrollZoom;
+            
+        // zoom out
+        }else {
+            state.scale *= scrollZoom;
+            movementSpeed *= scrollZoom;
+
+            // fix transition between singles and doubles
+            state.center[1] += movementSpeed / 100000.0;
+            state.center[0] += movementSpeed / 100000.0;
+        }
+
+    }
+}, true);
+
+// Mobile controls
+hammer
+    .on('panstart', (e) => {
+        prevCoord = {x: e.deltaX * panSens, y: e.deltaY * panSens};
+    })
+    .on('panmove', (e) => {
+        pos = {x: e.deltaX * panSens, y: e.deltaY * panSens};
+        state.center[0] += movementSpeed * (prevCoord.x - pos.x);
+        state.center[1] += movementSpeed * (pos.y - prevCoord.y);
+        prevCoord = pos
+    })
+    .on('pinchstart', (e) => {
+        baseScale = state.scale;
+        baseMs = movementSpeed;
+    })
+    .on('pinch', (e) => {
+        state.scale = baseScale / e.scale;
+        movementSpeed = baseMs / e.scale;
+    })
+
+// Render
+let avgFPS = 1.0;
+let compSpeed = 0.0;
+let compZoom = 0.0;
+
+app.ticker.add((delta) => {
+    avgFPS = 0.9 * avgFPS + (0.1 * app.ticker.FPS);
+    compSpeed = movementSpeed * delta * 2;
+    compZoom = Math.pow(zoom, delta * 2);
+    
+    if (showDebug){
+        coords.text = "renderer: " + renderer +
+                    "\n x: " + quad.shader.uniforms.center[0] +
+                    "\n ex: " + quad.shader.uniforms.center[1] +     
+                    "\n y: " + quad.shader.uniforms.center[2] + 
+                    "\n ey: " + quad.shader.uniforms.center[3] + 
+                    "\n s: " + quad.shader.uniforms.scale[0] + 
+                    "\n es: " + quad.shader.uniforms.scale[1] + 
+                    "\n iterations: " + quad.shader.uniforms.iterations +
+                    "\n mantissa: " + precision + " bit" +
+                    "\n fps: " + Math.floor(avgFPS);
+    } else coords.text = "";
+    
+    // Up
+    if (keyState[38]){
+        state.center[1] += compSpeed;
+    }
+    
+    // Down
+    if (keyState[40]){
+        state.center[1] -= compSpeed;
+    }
+    
+    // Left
+    if (keyState[39]){
+        state.center[0] += compSpeed;
+    }
+    
+    // Right
+    if (keyState[37]){
+        state.center[0] -= compSpeed;
+    }
+    
+    // Zoom in - Q
+    if (keyState[81]){
+        state.scale *= compZoom;
+        movementSpeed *= compZoom;
+    }
+    
+    // Zoom out - W
+    if (keyState[87]){
+        state.scale /= compZoom;
+        movementSpeed /= compZoom;
+    }
+    
+    // Increase iterations - A
+    if (keyState[65]){
+        quad.shader.uniforms.iterations += 2.0 * delta;
+    }
+    
+    // Decrease iterations - S
+    if (keyState[83]){
+        quad.shader.uniforms.iterations -= 2.0 * delta;
+    }
+    
+    quad.shader.uniforms.scale = split(state.scale);
+    quad.shader.uniforms.center = [...split(state.center[0]), ...split(state.center[1])];
+});
+
+// Only render when mouse over the canvas
+app.ticker.stop();
+app.ticker.update();
+
+app.view.onmouseover = () => {
+    app.ticker.start();
+    mouseover = true;
+    document.body.onmouseup = () => {
+        mousedown = false;
+    }
+}
+
+app.view.onmousedown = () => {
+    mousedown = true;
+}
+
+// stop rendering if out of canvas and not clicked down
+app.view.onmouseout = () => {
+    mouseover = false;
+    if (!mousedown) app.ticker.stop();
+    else {
+        document.body.onmouseup = () => {
+            app.ticker.stop();
+            mousedown = false;
+        }
+    }
+}
---
+++ js/shaders.js
@@ -0,0 +1,122 @@
+const fragment = `
+/* Adapted from http://andrewthall.org/papers/df64_qf128.pdf */
+
+precision highp float;
+
+uniform vec2 scale;
+uniform vec2 offset;
+uniform vec4 center;
+uniform vec2 zero;
+uniform float iterations;
+
+vec2 quickTwoSum(float a, float b){
+    float s = (a + b) + zero.x; // Stop compiler optimization
+    float e = b - (s - a);
+    
+    return vec2(s, e);
+}
+
+vec4 twoSumComp(vec2 a, vec2 b){
+    vec2 s = (a + b) + zero; // Stop compiler optimization
+    vec2 v = (s - a) + zero; // Stop compiler optimization
+    
+    vec2 e = (a - (s - v)) + (b - v);
+
+    return vec4(s.x, e.x, s.y, e.y);
+}
+
+vec2 df64_add(vec2 a, vec2 b){
+    vec4 st;
+    st = twoSumComp(a, b);
+    
+    st.y += st.z;
+    st.xy = quickTwoSum(st.x, st.y);
+    
+    st.y += st.w;
+    st.xy = quickTwoSum(st.x, st.y);
+ 
+    return st.xy;
+}
+
+vec2 df64_mult(vec2 a, vec2 b){
+    vec2 p;
+    
+    p = vec2(a.x * b.x, a.y * b.y);
+    p.y += a.x * b.y;
+    p.y += a.y * b.x;
+    p = quickTwoSum(p.x, p.y);
+    
+    return p;
+}
+
+vec3 get_color(float v, float c){
+    float x = v / c * 6.28318530718;
+
+    float R = 1.0 - cos(x * 5.8);
+    float G = 1.0 - cos(x * 5.5);
+    float B = 1.0 - cos(x * 5.2);
+ 
+    return vec3(R, G, B) * 0.5;
+}
+
+void main() {
+    
+    vec4 z = vec4(0.0); 
+    
+    // center + (gl_FragCoord.xy - offset) * scale
+    vec4 f = vec4(gl_FragCoord.x - offset.x, 0.0, gl_FragCoord.y - offset.y, 0.0);
+    vec4 s = vec4(df64_mult(f.xy, scale), df64_mult(f.zw, scale)); 
+    vec4 c = vec4(df64_add(center.xy, s.xy), df64_add(center.zw, s.zw));  
+  
+    vec3 color = vec3(0.0);
+    float iter = 0.0;
+    
+    for(int i = 0; i < 1024; i ++)
+    { 
+        if (iter > iterations) break;
+
+        // Real
+        vec2 real = df64_add(
+             df64_mult(z.xy, z.xy),
+            -df64_mult(z.zw, z.zw)
+        );
+
+        // Complex
+        vec2 complex = df64_mult(
+            vec2(2.0, 0.0),
+            df64_mult(z.zw, z.xy)
+        );
+
+        // z' = (z.z * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
+        z = vec4(df64_add(real, c.xy), df64_add(complex, c.zw));
+
+        // Escape time coloring
+        if (z.x * z.x + z.z * z.z >= 4.0) {
+            color = get_color(iter, iterations);
+            break;
+        }
+
+        iter += 1.0;
+    }
+
+    gl_FragColor = vec4(color, 1.0);
+}
+
+`;
+
+const vertex = `
+
+precision highp float;
+
+attribute vec2 aVertexPosition;
+
+uniform mat3 translationMatrix;
+uniform mat3 projectionMatrix;
+
+
+void main() {
+    gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+}
+
+`;
+
