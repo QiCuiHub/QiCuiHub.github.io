dfdcf1a6bc5c0b8e7397b386d60228c82e040472
QiCuiHub <qicuiqicui@yahoo.ca>
2021-01-03 06:44:53 -0500

    add supersampling

--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,32 +1,76 @@
 PIXI.settings.PRECISION_FRAGMENT = PIXI.PRECISION.HIGH
 
-// Split double into two singles
-let split = (a) => {
-    let b = a + a * 1e-7 * Math.random() // improve stability
-    let hi = Math.fround(b);
-    let lo = a - hi;
-    
-    return [hi, lo];
-}
-
-// State
 const resolution = {
     width: document.getElementById("app").offsetWidth + 1,
     height: document.getElementById("app").offsetHeight + 1
 }
 
-const state = {
-    center : [-0.5, 0.0],
-    scale  : 2.4 / Math.min(resolution.height, resolution.width)
+class MandelbrotMesh {
+    constructor(app, resolution, resolutionScale){
+        this.width  = resolution.width * resolutionScale
+        this.height = resolution.height * resolutionScale 
+
+        this.state = {
+            center     : [-0.5, 0.0],
+            scale      : 2.4 / Math.min(this.height, this.width),
+            iterations : 180        
+        }
+
+        this.movementSpeed = this.state.scale * resolutionScale;
+
+        let uniforms = {
+            center     : [
+                ...MandelbrotMesh._split(this.state.center[0]), 
+                ...MandelbrotMesh._split(this.state.center[1])
+            ],
+            offset     : [this.width / 2.0, this.height / 2.0],
+            scale      : MandelbrotMesh._split(this.state.scale),
+            zero       : [0.00000000000001, 0.00000000000001],
+            iterations : this.state.iterations,
+        }
+
+        // full canvas quad
+        let vertices = [
+            -this.width, -this.height, 
+             this.width, -this.height,
+             this.width,  this.height,
+            -this.width,  this.height
+        ]
+
+        let geometry = new PIXI.Geometry()
+            .addAttribute('aVertexPosition', vertices, 2) 
+            .addIndex([0, 1, 2, 0, 2, 3]);
+
+        let shader = PIXI.Shader.from(vertex, fragment, uniforms);
+        this.mesh = new PIXI.Mesh(geometry, shader, uniforms);
+    }
+
+    /**
+     * Split double into two singles
+     */
+    static _split(a) {
+        let b = a + a * 1e-7 * Math.random() // improve stability
+        let hi = Math.fround(b);
+        let lo = a - hi;
+        
+        return [hi, lo];
+    }
+
+    update() {
+        //console.log(this.test, this.state)
+        this.mesh.shader.uniforms.iterations = this.state.iterations;
+        this.mesh.shader.uniforms.scale = MandelbrotMesh._split(this.state.scale);
+        this.mesh.shader.uniforms.center = [
+            ...MandelbrotMesh._split(this.state.center[0]), 
+            ...MandelbrotMesh._split(this.state.center[1])
+        ];
+    }
+
+    resize(newW, newH) {
+        this.mesh.setTransform(0, 0, newW / this.resolution.width, newH / this.resolution.height);
+        this.mesh.shader.uniforms.offset = [newW / 2.0, newH / 2.0]
+    }
 }
-    
-const uniforms = {
-    center     : [...split(state.center[0]), ...split(state.center[1])],
-    offset     : [resolution.width / 2.0, resolution.height / 2.0],
-    scale      : split(state.scale),
-    zero       : [0.00000000000001, 0.00000000000001],
-    iterations : 256.0,
-};
 
 // App
 const app = new PIXI.Application(resolution);
@@ -34,38 +78,46 @@ const hammer = new Hammer(app.view);
 hammer.add( new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 }) );
 hammer.add( new Hammer.Pinch({ threshold: 0 }) );
 
-// Check Available Presicion and Renderer
+// Get debug info
 const gl = app.renderer.context.gl;
-const precision = gl.getShaderPrecisionFormat(gl.FRAGMENT_SHADER, gl.HIGH_FLOAT).precision;
 const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
 const renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
 
-// Canvas sized Quad
-const geometry = new PIXI.Geometry()
-    .addAttribute('aVertexPosition',
-        [-resolution.width, -resolution.height, 
-          resolution.width, -resolution.height,
-          resolution.width,  resolution.height,
-         -resolution.width,  resolution.height], 
-        2) 
-    .addIndex([0, 1, 2, 0, 2, 3]);
-
 // Add to canvas
-const shader = PIXI.Shader.from(vertex, fragment, uniforms);
-const quad = new PIXI.Mesh(geometry, shader, uniforms);
 const coords = new PIXI.Text('', {fontFamily : 'Arial', fontSize: 12, fill : 0x00ff00, align : 'left'});
+const baseMesh = new MandelbrotMesh(app, resolution, 1, true)
+const hdMesh = new MandelbrotMesh(app, resolution, 2, false)
 
-app.stage.addChild(quad);
+// hi res sprite for super sampling
+const SSAATexture = PIXI.RenderTexture.create({ width: hdMesh.width, height: hdMesh.height, scaleMode: PIXI.SCALE_MODES.LINEAR });
+const SSAASprite = new PIXI.Sprite(SSAATexture);
+SSAASprite.setTransform(0, resolution.height, 1, -1, 0, 0, 0, 0, 0)
+SSAASprite.width = resolution.width;
+SSAASprite.height = resolution.height;
+
+app.stage.addChild(baseMesh.mesh);
+app.stage.addChild(SSAASprite);
 app.stage.addChild(coords);
+
+// super sampling function
+let SSAAPass = () => {
+    app.renderer.render(hdMesh.mesh, SSAATexture);
+    SSAASprite.visible = true;
+    app.render()
+}
+
+let meshes = [baseMesh, hdMesh]
 
 // Movement
 let keyState = {};
-let mousedown = false;
+let keyStateLen = 0;
+
 let mouseover = false;
+let mousemoving = false;
+let scrolling = null;
 
 let zoom = 0.99;
 let scrollZoom = 0.84;
-let movementSpeed = state.scale;
 let showDebug = false;
 
 let prevCoord = null;
@@ -73,59 +125,92 @@ let baseScale = null;
 let baseMs = null
 
 window.addEventListener('keydown', (e) => {
-    keyState[e.keyCode || e.which] = true;
+    if (!((e.keyCode || e.which) in keyState)){
+        keyState[e.keyCode || e.which] = true
+        keyStateLen += 1
+    }
+
     if (e.keyCode == 68 || e.which == 68) showDebug = !showDebug;
     app.ticker.start();
 }, true);
 
 window.addEventListener('keyup', (e) => {
-    delete keyState[e.keyCode || e.which];
-    
-    // Stop rendering if no keys pressed and mouse not over canvas
-    if (Object.keys(keyState).length == 0 && !mouseover) app.ticker.stop();
+    if ((e.keyCode || e.which) in keyState){
+        delete keyState[e.keyCode || e.which]
+        keyStateLen -= 1
+    }
 },true);
 
 window.addEventListener('wheel', (e) => {
     // only scroll when mouse over canvas
     if (mouseover){
 
-        // zoom in
-        if (Math.sign(e.deltaY) > 0){
-            state.scale /= scrollZoom;
-            movementSpeed /= scrollZoom;
-            
-        // zoom out
-        }else {
-            state.scale *= scrollZoom;
-            movementSpeed *= scrollZoom;
+        if (scrolling === null && !app.ticker.started){
+            scrolling = setTimeout(() => {
+                scrolling = null
+                app.ticker.update()
+            }, 250);
         }
  
+        for (i in meshes) {
+            let mesh = meshes[i]
+
+            // zoom in
+            if (Math.sign(e.deltaY) > 0){
+                mesh.state.scale /= scrollZoom;
+                mesh.movementSpeed /= scrollZoom;
+                
+            // zoom out
+            }else {
+                mesh.state.scale *= scrollZoom;
+                mesh.movementSpeed *= scrollZoom;
+            }
+        }
+
+        app.ticker.update()
     }
 }, true);
+
+app.view.onmouseover = () => {mouseover = true}
+app.view.onmouseout = () => {mouseover = false}
 
 // Mobile controls
 hammer
     .on('panstart', (e) => {
-        app.ticker.start();
-        prevCoord = {x: e.deltaX, y: e.deltaY};
+        app.ticker.start()
+        prevCoord = {x: e.deltaX, y: e.deltaY}
+        mousemoving = true
     })
     .on('panmove', (e) => {
         pos = {x: e.deltaX, y: e.deltaY};
-        state.center[0] += movementSpeed * (prevCoord.x - pos.x);
-        state.center[1] += movementSpeed * (pos.y - prevCoord.y);
+        
+        for (i in meshes) {
+            let mesh = meshes[i]
+            mesh.state.center[0] += mesh.movementSpeed * (prevCoord.x - pos.x);
+            mesh.state.center[1] += mesh.movementSpeed * (pos.y - prevCoord.y);   
+        }
+            
         prevCoord = pos
     })
     .on('panend', (e) => {
         if (!mouseover) app.ticker.stop();
+        mousemoving = false;
     })
     .on('pinchstart', (e) => {
         app.ticker.start();
-        baseScale = state.scale;
-        baseMs = movementSpeed;
+
+        for (i in meshes) {
+            let mesh = meshes[i]        
+            mesh = mesh.state.scale;
+            mesh = mesh.movementSpeed;
+        }
     })
     .on('pinch', (e) => {
-        state.scale = baseScale / e.scale;
-        movementSpeed = baseMs / e.scale;
+        for (i in meshes) {
+            let mesh = meshes[i]           
+            mesh.state.scale = baseScale / e.scale;
+            mesh.movementSpeed = baseMs / e.scale;
+        }
     })
     .on('pinchend', (e) => {
         if (!mouseover) app.ticker.stop();
@@ -138,90 +223,77 @@ let compZoom = 0.0;
 
 app.ticker.add((delta) => {
     avgFPS = 0.9 * avgFPS + (0.1 * app.ticker.FPS);
-    compSpeed = movementSpeed * delta * 8;
-    compZoom = Math.pow(zoom, delta * 2);
-    
+
     if (showDebug){
         coords.text = "renderer: " + renderer +
-                    "\nx: " + state.center[0] +  
-                    "\ny: " + state.center[1] + 
-                    "\ns: " + state.scale + 
-                    "\niterations: " + Math.floor(quad.shader.uniforms.iterations) +
+                    "\nx: " + baseMesh.state.center[0] +
+                    "\ny: " + baseMesh.state.center[1] +
+                    "\ns: " + baseMesh.state.scale +
+                    "\niterations: " + Math.floor(baseMesh.state.iterations) +
                     "\nfps: " + Math.floor(avgFPS)
     } else coords.text = "";
     
-    // Up
-    if (keyState[38]){
-        state.center[1] += compSpeed;
+    for (i in meshes) {
+        let mesh = meshes[i]
+
+        compSpeed = mesh.movementSpeed * delta * 8;
+        compZoom = Math.pow(zoom, delta * 2);
+
+        // Up
+        if (keyState[38]){
+            mesh.state.center[1] += compSpeed;
+        }
+
+        // Down
+        if (keyState[40]){
+            mesh.state.center[1] -= compSpeed;
+        }
+        
+        // Left
+        if (keyState[39]){
+            mesh.state.center[0] += compSpeed;
+        }
+        
+        // Right
+        if (keyState[37]){
+            mesh.state.center[0] -= compSpeed;
+        }
+        
+        // Zoom in - Q
+        if (keyState[81]){
+            mesh.state.scale *= compZoom;
+            mesh.movementSpeed *= compZoom;
+        }
+        
+        // Zoom out - W
+        if (keyState[87]){
+            mesh.state.scale /= compZoom;
+            mesh.movementSpeed /= compZoom;
+        }
+        
+        // Increase iterations - A
+        if (keyState[65]){
+            mesh.state.iterations += delta;
+        }
+
+        // Decrease iterations - S
+        if (keyState[83]){
+            mesh.state.iterations -= delta;
+        }
+        
+        mesh.update()
     }
-    
-    // Down
-    if (keyState[40]){
-        state.center[1] -= compSpeed;
+
+    if (keyStateLen == 0 && !mousemoving && scrolling === null){
+        SSAAPass()
+        app.ticker.stop()
+
+    } else {
+        SSAASprite.visible = false
     }
-    
-    // Left
-    if (keyState[39]){
-        state.center[0] += compSpeed;
-    }
-    
-    // Right
-    if (keyState[37]){
-        state.center[0] -= compSpeed;
-    }
-    
-    // Zoom in - Q
-    if (keyState[81]){
-        state.scale *= compZoom;
-        movementSpeed *= compZoom;
-    }
-    
-    // Zoom out - W
-    if (keyState[87]){
-        state.scale /= compZoom;
-        movementSpeed /= compZoom;
-    }
-    
-    // Increase iterations - A
-    if (keyState[65]){
-        quad.shader.uniforms.iterations += delta;
-    }
-    
-    // Decrease iterations - S
-    if (keyState[83]){
-        quad.shader.uniforms.iterations -= delta;
-    }
-    
-    quad.shader.uniforms.scale = split(state.scale);
-    quad.shader.uniforms.center = [...split(state.center[0]), ...split(state.center[1])];
 });
 
-// Start render when mouse over the canvas
-app.ticker.stop();
-
-app.view.onmouseover = () => {
-    app.ticker.start();
-    mouseover = true;
-    document.body.onmouseup = () => {
-        mousedown = false;
-    }
-}
-
-app.view.onmousedown = () => {
-    mousedown = true;
-}
-
-// stop rendering if out of canvas and not clicked down
-app.view.onmouseout = () => {
-    mouseover = false;
-    if (!mousedown) app.ticker.stop();
-    else {
-        document.body.onmouseup = () => {
-            app.ticker.stop();
-            mousedown = false;
-        }
-    }
-}
+app.ticker.stop()
 
 // resize app on window resize
 window.addEventListener("resize", () => {
@@ -229,9 +301,7 @@ window.addEventListener("resize", () => {
     let newH = document.getElementById("app").offsetHeight + 1;
 
     app.renderer.resize(newW, newH);
-    quad.setTransform(0, 0, newW / resolution.width, newH / resolution.height);
-    quad.shader.uniforms.offset = [newW / 2.0, newH / 2.0]
-    
+    baseMesh.resize(newW, newH);
     app.ticker.update();  
 });
 
@@ -242,9 +312,9 @@ while (appElement.firstChild) {
     appElement.removeChild(appElement.firstChild);
 }
 
-appElement.appendChild(app.view);
-appElement.onwheel = e => e.preventDefault();
-app.render();
+appElement.appendChild(app.view)
+appElement.onwheel = e => e.preventDefault()
+SSAAPass()
 
 // ios not loading sometimes need rerender
-setTimeout(() => {app.render()}, 1000);
+setTimeout(() => {SSAAPass}, 1000);
